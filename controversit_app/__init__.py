8b9ff91df9be529b111f01b45849da31ed2d7dcb
Davide Martizzi <dmartizzi@Davides-MBP.hsd1.ca.comcast.net>
2019-09-22 09:43:52 -0700

    1. Developed production version of the pipeline to create NLP and regression models.
    2. Developed a web app that uses the models to make predictions.

---
+++ controversit_app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+from controversit_app import views
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 300
+# No cacheing at all for API endpoints.
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
---
+++ run.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+from controversit_app import app
+app.run(debug = True)
